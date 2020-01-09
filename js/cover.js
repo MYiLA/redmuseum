f4429bbf9486df6bacb2fe7cbaf73e19e7d59861
MYiLA <polus180@ya.ru>
2020-01-09 14:36:48 +0300

    fix link

--- js/cover.js
+++ js/cover.js
@@ -1,1 +1,1 @@
-"use strict";var logoBtnElement=document.querySelector(".cover__link"),goHomePage=function(){location.href="/blog.html"};logoBtnElement.addEventListener("click",function(e){e.preventDefault(),logoBtnElement.style.animation="disappear 1s forwards",setTimeout(goHomePage,1e3)});
+"use strict";var logoBtnElement=document.querySelector(".cover__link"),goHomePage=function(){location.href="blog.html"};logoBtnElement.addEventListener("click",function(e){e.preventDefault(),logoBtnElement.style.animation="disappear 1s forwards",setTimeout(goHomePage,1e3)});
